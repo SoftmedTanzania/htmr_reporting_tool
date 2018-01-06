5b05090751091f89dfe8ee938420a0987ac064ed
Leonard C. Mpande <leo.agust27@gmail.com>
2018-01-06 11:35:00 +0300

    Creating location service

--- src/app/shared/services/location.service.ts
+++ src/app/shared/services/location.service.ts
@@ -1,16 +1,41 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClientService} from './http-client.service';
 import {Location} from '../models/location';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class LocationService {
 
-  locataion: Location[];
-  constructor(private http: HttpClientService) { }
+  locations: Location[];
+
+  constructor(private http: HttpClientService) {
+  }
 
   // get all data element group
   loadLocations() {
-    return this.http.getOpenMRS(`location`);
+
+    return Observable.create(observer => {
+
+      this.http.getOpenMRS(`location`)
+        .subscribe((locationResponse: any) => {
+
+            this.locations = locationResponse.results.map((location) => {
+              return {
+                uuid: location.uuid,
+                display: location.display,
+                links: location.links
+              };
+            });
+
+            observer.next(this.locations);
+            observer.complete();
+          },
+          error => {
+            observer.error('some error occur');
+          });
+
+    });
+
   }
 
 
