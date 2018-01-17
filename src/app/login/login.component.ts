e4b91f996332b5b85482ed3c96329d0727e96126
Leonard C. Mpande <leo.agust27@gmail.com>
2018-01-17 06:58:06 +0300

    modified login notifications

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,8 +10,10 @@ import {UserService} from '../shared/services/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginNotification = {isError: false, message: 'Login successful', attempted: false};
+  loginNotification = {isError: false, message: 'Login successful', attempted: false, loading: false};
   loginForm: FormGroup;
+  customeUsernameNotifier: boolean = false;
+  customePasswordNotifier: boolean = false;
 
   constructor(private store: Store<ApplicationState>, private userService: UserService, private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group(
@@ -19,6 +21,7 @@ export class LoginComponent implements OnInit {
         username: ['', Validators.required],
         password: ['', Validators.required]
       });
+    console.log(this.loginForm);
   }
 
   ngOnInit() {
@@ -26,12 +29,24 @@ export class LoginComponent implements OnInit {
   }
 
   login(loginForm) {
+    this.customeUsernameNotifier = false;
+    this.customePasswordNotifier = false;
     const loginCredentials = loginForm.value;
 
+    if (loginCredentials.username === '') {
+      this.customeUsernameNotifier = true;
+      return;
+    } else if (loginCredentials.password === '') {
+      this.customePasswordNotifier = true;
+      return;
+    }
+
+    this.loginNotification.loading = true;
     this.userService.login(loginCredentials).subscribe((results) => {
       this.loginNotification.isError = false;
       this.loginNotification.message = 'Login successful';
       this.loginNotification.attempted = true;
+      this.loginNotification.loading = false;
       setTimeout(() => {
         this.store.dispatch(new Go({path: ['/home', 'dashboard']}));
       }, 2000);
@@ -40,6 +55,7 @@ export class LoginComponent implements OnInit {
       this.loginNotification.isError = true;
       this.loginNotification.message = 'Login failure, wrong username or password';
       this.loginNotification.attempted = true;
+      this.loginNotification.loading = false;
       this.store.dispatch(new Go({path: ['/login']}));
     });
   }
